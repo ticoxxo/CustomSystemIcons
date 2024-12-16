466808c5f6e81526f5a95ee640a69d9564b6c961
Ticoxxo <ticoxxo@gmail.com>
2024-12-16 15:54:21 -0700

    Initial Commit

---
+++ CustomSystemIcons/CustomSystemIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSystemIconsApp.swift
+//  CustomSystemIcons
+//
+//  Created by Alberto Almeida on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSystemIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
